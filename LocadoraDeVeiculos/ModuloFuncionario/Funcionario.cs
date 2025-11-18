7bf54d8252ccedc977ef36808d060a0f817e2294
LeziinFx <leandronunesramosln@gmail.com>
2025-11-18 14:55:54 -0300

    Add project files.

---
+++ LocadoraDeVeiculos/ModuloFuncionario/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.ModuloFuncionario
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public DateOnly DataAdmissao { get; set; }
+        public decimal salario { get; set; }
+
+        public Funcionario(string nome, DateOnly dataAdmissao, decimal salario)
+        {
+            Nome = nome;  
+            DataAdmissao = dataAdmissao;  
+            this.salario = salario;
+        }
+    }
+}
